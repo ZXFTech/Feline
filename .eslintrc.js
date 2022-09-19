dd6d4b123a5497c7066c1ddfaa554b593a3cf4f1
Feline <zxf_tech@outlook.com>
2022-09-19 23:00:57 +0800

    chore(config): 添加 vite 构建工具,添加tsconfig.
    添加并配置vite打包工具.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,10 +30,11 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    indent: ["error", "space"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    curly: ["error", "multi", "consistent"],
+    indent: ["error", "space"], // Tab 使用两个空格
+    "linebreak-style": ["error", "unix"], // 换行符统一使用 \n
+    quotes: ["error", "double"], // 所有字符串使用双引号
+    semi: ["error", "always"], // 语句结尾必须带分号
+    curly: ["error", "multi", "consistent"], // if 语句严格带大括号
+    "no-unser-var": ["error", "true"], // 不能有声明后未使用的变量或参数
   },
 };
