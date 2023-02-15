f93ca2d06052d9a7d7c121fe504d70491e9faa57
Feline <zxf_tech@outlook.com>
2023-02-15 16:44:06 +0800

    feat(utils): 添加 redux 组件

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 import TopBar from "./src/components/TopBar";
 import NeuContainer from "./src/components/NeuTest";
+import Counter from "@/page/ReduxTest";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+  },
+  {
+    path: "/post/:postId",
+  },
+  {
+    path: "/editPost/:postId",
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <TopBar />
-      <div className="content-area">
-        <div className="left-func-zone"></div>
-        <div className="main-display-zone"></div>
-        <div className="right-func-zone"></div>
-      </div>
-      <div className="bottom-bar"></div>
-      {/* <NeuContainer /> */}
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
