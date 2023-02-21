b9858bd9aef8f2936969d52ba609d17f1aec56fd
Feline <zxf_tech@outlook.com>
2023-02-21 23:10:33 +0800

    feat(components): 建设中组件1.0

---
+++ src/components/Constructing/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import NeuContainer from "@/components/NeuContainer";
+
+import "./index.css";
+
+const Constructing = () => {
+  return (
+    <NeuContainer className="constructing-container">
+      <div>建设中...</div>
+      <div>敬请期待</div>
+      <div>Building...</div>
+    </NeuContainer>
+  );
+};
+
+export default Constructing;
