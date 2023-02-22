6bd701fd1e1a70d097d56f9cc609e2510bb945b0
Feline <zxf_tech@outlook.com>
2023-02-22 10:26:47 +0800

    refactor(components): 建设中组件样式调整

--- src/components/Constructing/index.tsx
+++ src/components/Constructing/index.tsx
@@ -6,11 +6,11 @@ import "./index.css";
 
 const Constructing = () => {
   return (
-    <NeuContainer className="constructing-container">
+    <div className="constructing-container">
       <div>建设中...</div>
       <div>敬请期待</div>
       <div>Building...</div>
-    </NeuContainer>
+    </div>
   );
 };
 
