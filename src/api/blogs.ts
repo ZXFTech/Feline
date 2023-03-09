c4e6ccb289de0c877dfbaca38fc4fdabfb83f9a3
Feline <zxf_tech@outlook.com>
2023-03-09 12:55:40 +0800

    feat(utils):
    添加博客api

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -1,12 +1,18 @@
-import { get } from "@/request";
-import { blogDetail, blogList } from "./url";
+import { get, post } from "@/request";
+import { FBlog } from "@/types/blogs";
+import { urlBlogDetail, urlBlogList, urlAddBlog } from "./url";
 
 // 获取博客列表
 export const getBlogList = (pageNum: number, pageSize: number) => {
-  return get(blogList, { pageNum, pageSize });
+  return get(urlBlogList, { pageNum, pageSize });
 };
 
 // 获取博客内容
-export const getBLogDetail = (blogId: number) => {
-  return get(blogDetail, { blogId });
+export const getBLogDetail = (blogId: number | string) => {
+  return get(urlBlogDetail, { id: blogId });
+};
+
+// 添加博客
+export const addBlog = (blog: FBlog) => {
+  return post(urlAddBlog, blog);
 };
