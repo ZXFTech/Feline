bf7e2950fb43533da85c98539e15b638bba5e5fc
Feline <zxf_tech@outlook.com>
2023-03-15 21:32:56 +0800

    feat(components): 增加博客接口请求测试逻辑

--- src/components/Blog/BlogList.tsx
+++ src/components/Blog/BlogList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { getBlogList } from "@/api/blogs";
+import { addBlog, getBlogList } from "@/api/blogs";
 import { FBlog } from "@/types/blogs";
 
 import JSONBlogList from "./blogList.json";
@@ -8,8 +8,7 @@ import Breviary from "./Breviary";
 
 import "./index.css";
 import Pagination from "../Pagination";
-
-console.log("JSONBlogList", JSONBlogList);
+import Loading from "../Loading";
 
 export const BlogList = () => {
   const [loading, setLoading] = useState(false);
@@ -19,31 +18,63 @@ export const BlogList = () => {
   const [pageSize, setPageSize] = useState(20);
 
   const [pageNum, setPageNum] = useState(1);
+  const [total, setTotal] = useState(0);
 
-  useEffect(() => {
-    setLoading(true);
-
-    getBlogList(pageNum, pageSize)
+  const onBlogUpload = () => {
+    const blog = {
+      title: "吃饭",
+      content: `
+      \`\`\`javascript
+      12300几分钟哦放假哦阿姐大佛\n
+      \`\`\`
+      `,
+      author: "feline",
+      tags: "react javascript food",
+      likes: 10,
+    };
+    addBlog(blog)
       .then((res) => {
         console.log("res", res);
-        setBlogList(JSONBlogList);
       })
       .catch((err) => {
         console.log("err", err);
       });
+  };
+
+  const onBLogUpdate = () => {
+    setLoading(true);
+    getBlogList(pageNum, pageSize)
+      .then((res: any) => {
+        setBlogList(res.data.list);
+        setTotal(res.data.total);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    onBLogUpdate();
   }, []);
 
   return (
     <div>
-      <div className="list-container">
-        {blogList.length ? (
-          blogList.map((blog, index: number) => {
-            return <Breviary key={blog.blogId} blog={blog} />;
-          })
-        ) : (
-          <div>暂无博客</div>
-        )}
-      </div>
+      <button onClick={onBlogUpload}>上传</button>
+      <button onClick={onBLogUpdate}>刷新</button>
+      <Loading visible={loading}>
+        <div className="list-container">
+          {blogList.length ? (
+            blogList.map((blog, index: number) => {
+              return <Breviary key={blog.id} blog={blog} />;
+            })
+          ) : (
+            <div>暂无博客</div>
+          )}
+        </div>
+      </Loading>
 
       <div>
         <Pagination />
