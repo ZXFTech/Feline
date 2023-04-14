743201fe2fd563e82cd6ae145bdfb5fc7e8144f4
Feline <zxf_tech@outlook.com>
2023-04-14 18:00:43 +0800

    feat(components): 导航栏 0.0.1 版本

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import Theme from "../Theme";
+
+import style from "./index.module.scss";
+import NeuContainer from "../NeuContainer";
 
 // 左侧 图标加名称
 // 右侧 导航栏
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <div className="icon-name-container"></div>
-      <div className="navbar">
-        <ul>
-          <li className="link">主页</li>
-          <li className="link">博文</li>
-          <li className="link">时间线</li>
-          <li className="link">分类</li>
-        </ul>
+    <NeuContainer type="protuberant" className={style["navbar-container"]}>
+      <div className={style["icon"]}>FELINE</div>
+      <div className={style.navbar}>
+        <NeuContainer
+          hover="protuberant"
+          className={style.link}
+          style={{ "--i": 1 } as CSSProperties}
+        >
+          主页
+        </NeuContainer>
+        <NeuContainer
+          hover="protuberant"
+          className={style.link}
+          style={{ "--i": 2 } as CSSProperties}
+        >
+          博文
+        </NeuContainer>
+        <NeuContainer
+          hover="protuberant"
+          className={style.link}
+          style={{ "--i": 3 } as CSSProperties}
+        >
+          时间线
+        </NeuContainer>
+        <NeuContainer
+          hover="protuberant"
+          className={style.link}
+          style={{ "--i": 4 } as CSSProperties}
+        >
+          分类
+        </NeuContainer>
+        <Theme />
       </div>
-    </div>
+    </NeuContainer>
   );
 };
 
