7017d7c09ebd1179f4092e9956d4d7880cac0881
Feline <zxf_tech@outlook.com>
2023-04-27 21:05:23 +0800

    refactor(components): 调整 constructing 组件的展示

--- src/components/Constructing/index.tsx
+++ src/components/Constructing/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import NeuContainer from "@/components/NeuContainer";
-
-import "./index.css";
+import style from "./index.module.scss";
 
 const Constructing = () => {
   return (
-    <div className="constructing-container">
+    <div className={style["constructing-container"]}>
       <div>建设中...</div>
       <div>敬请期待</div>
       <div>Building...</div>
