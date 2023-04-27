bf4f8f29301dbaaad777a75fad263d43e99745c3
Feline <zxf_tech@outlook.com>
2023-04-27 20:59:57 +0800

    refactor(components): 重新调整 neuTest 的展示逻辑

--- src/components/NeuTest/index.tsx
+++ src/components/NeuTest/index.tsx
@@ -4,7 +4,7 @@ import imgUrl from "../../assets/avatar.jpg";
 
 import NeuContainer from "../NeuContainer";
 
-import style from "./index.module.css";
+import style from "./index.module.scss";
 
 // type 描述整体突起还是塌陷  突起 | 塌陷 | 平坦(默认)
 // surface 描述表面突起还是塌陷 突起 | 塌陷 | 平坦(默认)
@@ -12,7 +12,7 @@ import style from "./index.module.css";
 
 const NeuTest = ({}) => {
   return (
-    <div className="test-container">
+    <div className={style["test-container"]}>
       <NeuContainer type="sunken">
         <div>平面塌陷</div>
       </NeuContainer>
@@ -25,76 +25,37 @@ const NeuTest = ({}) => {
         <div>聚焦凸起</div>
       </NeuContainer>
 
-      <div className="common-container hover-sunken">
+      <NeuContainer type="common" hover="sunken">
         <div>平面平坦</div>
         <div>聚焦塌陷</div>
-      </div>
-      <div className="common-container hover-common">
+      </NeuContainer>
+      <NeuContainer type="common">
         <div>平面平坦</div>
         <div>聚焦平坦</div>
-      </div>
-      <div className="common-container hover-protuberant">
+      </NeuContainer>
+      <NeuContainer type="common" hover="protuberant">
         <div>平面平坦</div>
         <div>聚焦凸起</div>
-      </div>
+      </NeuContainer>
 
-      <div className="protuberant-container hover-sunken">
+      <NeuContainer type="protuberant" hover="sunken">
         <div>平面突起</div>
         <div>聚焦塌陷</div>
-      </div>
-      <div className="protuberant-container hover-common">
+      </NeuContainer>
+      <NeuContainer
+        type="protuberant"
+        hover="common"
+        intensity={50}
+        visualHeight={10}
+        animation
+      >
         <div>平面突起</div>
         <div>聚焦平坦</div>
-      </div>
-      <div className="protuberant-container hover-protuberant">
+      </NeuContainer>
+      <NeuContainer type="protuberant">
         <div>平面突起</div>
         <div>聚焦凸起</div>
-      </div>
-
-      {/* 凸面 */}
-      <div className="protuberant-container convex ">
-        <div>凸面突起</div>
-      </div>
-      <div className="protuberant-container convex  hover-protuberant hover-flat">
-        <div>凸面突起</div>
-        <div>聚焦平面还原</div>
-      </div>
-      <div className="protuberant-container convex hover-concave hover-protuberant  ">
-        <div>平面突起</div>
-        <div>聚焦平面凹陷</div>
-      </div>
-
-      {/* 凹面 */}
-      <div className="protuberant-container concave">
-        <div>凹面突起</div>
-      </div>
-      <div className="protuberant-container concave hover-protuberant hover-flat">
-        <div>凹面突起</div>
-        <div>聚焦平面</div>
-      </div>
-      <div className="protuberant-container concave hover-protuberant hover-convex">
-        <div>凹面突起</div>
-        <div>聚焦凸起</div>
-      </div>
-
-      {/* <div className="flat-surface-container">
-        <div>平面突起</div>
-      </div>
-      <div className="flat-surface-container hover-flat">
-        <div>平面突起</div>
-        <div>聚焦平坦</div>
-      </div>
-      <div className="flat-surface-container hover-sunken">
-        <div>平面突起</div>
-        <div>聚焦凹陷</div>
-      </div>
-      <div className="animate1 ">
-        <div className="container">
-          <img src={imgUrl} className="img" />
-        </div>
-      </div>
-      <div className="animate2 ">animate2</div>
-      <div className="sunken-surface-container ">sunken surface</div> */}
+      </NeuContainer>
     </div>
   );
 };
