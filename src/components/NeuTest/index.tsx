3f8c554c11cb2df32c4c40377ef6e9a1d43eb4be
Feline <zxf_tech@outlook.com>
2023-05-03 22:02:12 +0800

    feat(components): neuTest 测试 button/input 代码

--- src/components/NeuTest/index.tsx
+++ src/components/NeuTest/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import imgUrl from "../../assets/avatar.jpg";
 
 import NeuContainer from "../NeuContainer";
 
 import style from "./index.module.scss";
+import NeuButton from "../NeuContainer/NeuButton";
+import NeuInput from "../NeuContainer/NeuInput";
 
 // type 描述整体突起还是塌陷  突起 | 塌陷 | 平坦(默认)
 // surface 描述表面突起还是塌陷 突起 | 塌陷 | 平坦(默认)
 // hover 鼠标移动到元素时的表现 突起 | 塌陷 | 表面突起 | 表面塌陷 | 无(默认)
 
 const NeuTest = ({}) => {
+  const [text, setText] = useState("second");
+
+  const onChange = (value) => {
+    console.log("value", value);
+  };
   return (
     <div className={style["test-container"]}>
-      <NeuContainer type="sunken">
+      <NeuButton size="large">按钮测试</NeuButton>
+      <NeuInput inputType="number" onChange={onChange} />
+      {/* <NeuContainer type="sunken">
         <div>平面塌陷</div>
       </NeuContainer>
       <NeuContainer type="sunken" hover="common">
@@ -55,7 +64,7 @@ const NeuTest = ({}) => {
       <NeuContainer type="protuberant">
         <div>平面突起</div>
         <div>聚焦凸起</div>
-      </NeuContainer>
+      </NeuContainer> */}
     </div>
   );
 };
